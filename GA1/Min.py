ffd7194359fe8c21f8d293b2a2279db92d81f311
Flameis <Rocketgunslinger@gmail.com>
2024-10-11 12:55:14 -0700

    Initial commit

---
+++ GA1/Min.py
@@ -0,0 +1,25 @@
+'''
+This file contains the template for Assignment1. For testing it, I will place
+it
+in a different directory, call the function <min_num_attendees>, and check its
+output.
+So, you can add/remove whatever you want to/from this file. But, don't change
+the name
+of the file or the name/signature of the following function.
+Also, I will use <python3> to run this code.
+'''
+def min_num_attendees(input_file_path, output_file_path):
+    '''
+    This function will contain your code. It wil read from the file
+    <input_file_path>,
+    and will write its output to the file <output_file_path>.
+    '''
+    pass
+'''
+To test your function, you can uncomment the following command with the the
+input/output
+files paths that you want to read from/write to. Do NOT forget to comment it
+out before
+submitting.
+'''
+min_num_attendees('', '')
